87b7b1a052217b105e0e3ac555063c11e9e72213
Klyd <anthony.tang@etu.univ-savoie.fr>
2013-11-19 11:55:46 +0100

    Ca devrait aller

--- src/engine/VmMaker.java
+++ src/engine/VmMaker.java
@@ -5,6 +5,6 @@ public class VmMaker {
 	 * Constructeur
 	 */
 	public VmMaker(){
-		System.out.println("Eh franchement j'fais pas expres");
+		System.out.println("Eh franchement j'fais expres");
 	}
 }
