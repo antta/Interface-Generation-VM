a33428490e465beb463d61d3722a3c0975fe8af7
Klyd <anthony.tang@etu.univ-savoie.fr>
2013-11-20 08:21:42 +0100

    retrait Syso VmMaker, retrait ligne XMLParser

--- src/engine/VmMaker.java
+++ src/engine/VmMaker.java
@@ -5,6 +5,5 @@ public class VmMaker {
 	 * Constructeur
 	 */
 	public VmMaker(){
-		System.out.println("Eh franchement j'fais expres");
 	}
 }
--- src/engine/XMLParser.java
+++ src/engine/XMLParser.java
@@ -4,6 +4,5 @@ public class XMLParser {
 	
 	public XMLParser() {
 		//constructor
-		
 	}
 }
