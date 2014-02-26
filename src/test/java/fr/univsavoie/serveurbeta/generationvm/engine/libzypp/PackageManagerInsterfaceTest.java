03156f2a85aef545487486d848a5b89d8380991b
patrick-edouard <proux@etu.univ-savoie.fr>
2014-02-26 11:53:14 +0100

    Rectification d'une faute de frappe dans le nom de classe

--- src/test/java/fr/univsavoie/serveurbeta/generationvm/engine/libzypp/PackageManagerInsterfaceTest.java
+++ src/test/java/fr/univsavoie/serveurbeta/generationvm/engine/libzypp/PackageManagerInsterfaceTest.java
@@ -1,17 +1,17 @@
 package fr.univsavoie.serveurbeta.generationvm.engine.libzypp;
 
 import junit.framework.TestCase;
-
+import fr.univsavoie.serveurbeta.generationvm.engine.libzypp.*;
 import java.util.ArrayList;
 
 /**
  * Created by patrick-edouard on 2/19/14.
  */
-public class PackageManagerInsteraceTest extends TestCase {
+public class PackageManagerInsterfaceTest extends TestCase {
     public void testGetProposedPackagesName() throws Exception {
         String packageName = "kiwi";
 
-        PackageManagerInsterace pm = new PackageManagerInsterace();
+        PackageManagerInterface pm = new PackageManagerInterface();
         ArrayList<String> packagesName = pm.getProposedPackagesName(packageName);
         assertEquals("kiwi",packagesName.get(0));
         assertEquals("kiwi-config-openSUSE",packagesName.get(1));
